c3f886f5129db290665925d4c8edf940321027a8
nathan <nathan@appflowy.io>
2023-09-06 15:18:06 +0800

    chore: update

--- collab-plugins/src/lib.rs
+++ collab-plugins/src/lib.rs
@@ -1,11 +1,8 @@
 #[cfg(any(feature = "rocksdb_plugin"))]
 pub use collab_persistence::*;
 
-mod sync_plugin;
-
-pub mod sync {
-  pub use crate::sync_plugin::*;
-}
+#[cfg(feature = "sync_plugin")]
+pub mod sync_plugin;
 
 pub mod local_storage;
 
