4f09fdbc0bb008bb95b6003bbcf8cc68eb03dd3c
nathan <nathan@appflowy.io>
2024-01-06 10:38:29 +0800

    chore: compress type

--- collab-plugins/src/local_storage/rocksdb/kv_impl.rs
+++ collab-plugins/src/local_storage/rocksdb/kv_impl.rs
@@ -31,8 +31,8 @@ impl RocksStore {
     db_opts.set_max_background_jobs(4);
     db_opts.set_compression_type(rocksdb::DBCompressionType::Zstd);
     db_opts.set_recycle_log_file_num(5);
-    db_opts.set_keep_log_file_num(10);
-    db_opts.set_db_log_dir("logs");
+    db_opts.set_keep_log_file_num(5);
+    db_opts.set_db_log_dir(path.as_ref().join("logs"));
     db_opts.create_if_missing(true);
 
     let open_result = TransactionDB::<SingleThreaded>::open(&db_opts, &txn_db_opts, &path);
