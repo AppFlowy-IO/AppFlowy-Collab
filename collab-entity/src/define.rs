c9670ddf034339d6d0ee3d3072e064cfba16e5f4
nathan <nathan@appflowy.io>
2024-03-30 09:56:53 +0800

    chore: add missing file

---
+++ collab-entity/src/define.rs
@@ -0,0 +1,13 @@
+// Document
+pub const DOCUMENT_ROOT: &str = "document";
+
+// Folder
+pub const FOLDER: &str = "folder";
+pub const FOLDER_META: &str = "meta";
+pub const FOLDER_CURRENT_WORKSPACE: &str = "current_workspace";
+
+// Database
+pub const WORKSPACE_DATABASES: &str = "databases";
+pub const DATABASE: &str = "database";
+pub const DATABASE_ID: &str = "id";
+pub const DATABASE_ROW_DATA: &str = "data";
