a6b472abfe23b9d0dac626274c1f2477519cd781
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-04-23 11:30:48 +0800

    chore: update logs (#207)

--- collab-folder/tests/folder_test/serde_test.rs
+++ collab-folder/tests/folder_test/serde_test.rs
@@ -1,5 +1,12 @@
-use collab_folder::{timestamp, UserId};
+use collab::core::collab::MutexCollab;
+use collab::core::origin::CollabOrigin;
+use collab::preclude::Collab;
+use collab_folder::{timestamp, Folder, FolderData, UserId};
+use parking_lot::Mutex;
 use serde_json::json;
+use std::ops::Deref;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
 
 use crate::util::{create_folder, make_test_view};
 
@@ -200,3 +207,120 @@ async fn child_view_json_serde() {
     }
   }));
 }
+
+#[tokio::test]
+async fn deserialize_folder_data() {
+  let json = include_str!("../folder_test/history_folder/folder_data.json");
+  let folder_data: FolderData = serde_json::from_str(json).unwrap();
+  let collab = Arc::new(MutexCollab::new(Collab::new_with_origin(
+    CollabOrigin::Empty,
+    "1",
+    vec![],
+    true,
+  )));
+  let folder = MutexFolder::new(Folder::create(1, collab.clone(), None, folder_data));
+
+  let mut handles = vec![];
+  for _ in 0..40 {
+    let clone_folder = folder.clone();
+    let handle = tokio::spawn(async move {
+      let start = Instant::now();
+      let _trash_ids = clone_folder
+        .lock()
+        .get_all_trash_sections()
+        .into_iter()
+        .map(|trash| trash.id)
+        .collect::<Vec<String>>();
+
+      // get the private view ids
+      let _private_view_ids = clone_folder
+        .lock()
+        .get_all_private_sections()
+        .into_iter()
+        .map(|view| view.id)
+        .collect::<Vec<String>>();
+
+      get_view_ids_should_be_filtered(&clone_folder.lock());
+      let elapsed = start.elapsed();
+      Ok::<Duration, anyhow::Error>(elapsed)
+    });
+    handles.push(handle);
+  }
+
+  let results = futures::future::join_all(handles).await;
+  for result in results {
+    let elapsed = result.unwrap();
+    println!("Time elapsed is: {:?}", elapsed);
+  }
+}
+
+fn get_view_ids_should_be_filtered(folder: &Folder) -> Vec<String> {
+  let trash_ids = get_all_trash_ids(folder);
+  let other_private_view_ids = get_other_private_view_ids(folder);
+  [trash_ids, other_private_view_ids].concat()
+}
+
+fn get_other_private_view_ids(folder: &Folder) -> Vec<String> {
+  let my_private_view_ids = folder
+    .get_my_private_sections()
+    .into_iter()
+    .map(|view| view.id)
+    .collect::<Vec<String>>();
+
+  let all_private_view_ids = folder
+    .get_all_private_sections()
+    .into_iter()
+    .map(|view| view.id)
+    .collect::<Vec<String>>();
+
+  all_private_view_ids
+    .into_iter()
+    .filter(|id| !my_private_view_ids.contains(id))
+    .collect()
+}
+
+fn get_all_trash_ids(folder: &Folder) -> Vec<String> {
+  let trash_ids = folder
+    .get_all_trash_sections()
+    .into_iter()
+    .map(|trash| trash.id)
+    .collect::<Vec<String>>();
+  let mut all_trash_ids = trash_ids.clone();
+  for trash_id in trash_ids {
+    all_trash_ids.extend(get_all_child_view_ids(folder, &trash_id));
+  }
+  all_trash_ids
+}
+
+fn get_all_child_view_ids(folder: &Folder, view_id: &str) -> Vec<String> {
+  let child_view_ids = folder
+    .views
+    .get_views_belong_to(view_id)
+    .into_iter()
+    .map(|view| view.id.clone())
+    .collect::<Vec<String>>();
+  let mut all_child_view_ids = child_view_ids.clone();
+  for child_view_id in child_view_ids {
+    all_child_view_ids.extend(get_all_child_view_ids(folder, &child_view_id));
+  }
+  all_child_view_ids
+}
+
+#[derive(Clone)]
+#[allow(clippy::arc_with_non_send_sync)]
+struct MutexFolder(Arc<Mutex<Folder>>);
+
+impl MutexFolder {
+  #[allow(clippy::arc_with_non_send_sync)]
+  fn new(folder: Folder) -> Self {
+    Self(Arc::new(Mutex::new(folder)))
+  }
+}
+impl Deref for MutexFolder {
+  type Target = Arc<Mutex<Folder>>;
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+unsafe impl Sync for MutexFolder {}
+unsafe impl Send for MutexFolder {}
