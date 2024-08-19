6001578e7e7d6267a71312db1379734791651487
nathan <nathan@appflowy.io>
2024-08-19 21:39:39 +0800

    chore: create row

--- collab-database/src/database.rs
+++ collab-database/src/database.rs
@@ -376,6 +376,11 @@ impl Database {
     self.body.block.create_new_database_row(row_id.clone())
   }
 
+  #[instrument(level = "debug", skip_all)]
+  pub fn init_database_row(&self, row_id: &RowId) -> Option<Arc<RwLock<DatabaseRow>>> {
+    self.body.block.get_or_init_row(row_id.clone())
+  }
+
   pub fn get_database_row(&self, row_id: &RowId) -> Option<Arc<RwLock<DatabaseRow>>> {
     self.body.block.get_row(row_id)
   }
