b864751dc54c6873d72b368e5cff5e72d9f54da4
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2024-08-29 08:03:25 +0200

    chore: pushed timeouts out of the collab struct

--- collab/src/lock.rs
+++ collab/src/lock.rs
@@ -4,53 +4,52 @@ use std::time::Duration;
 pub const DEFAULT_RWLOCK_TIMEOUT: Duration = Duration::from_secs(15);
 
 #[derive(Debug)]
+#[repr(transparent)]
 pub struct RwLock<T: ?Sized> {
-  inner: Box<tokio::sync::RwLock<T>>,
-  timeout: Duration,
+  inner: tokio::sync::RwLock<T>,
 }
 
 impl<T: ?Sized> RwLock<T> {
-  pub fn new(inner: T, timeout: Duration) -> Self
+  pub fn new(inner: T) -> Self
   where
     T: Sized,
   {
     Self {
-      inner: tokio::sync::RwLock::new(inner).into(),
-      timeout,
+      inner: tokio::sync::RwLock::new(inner),
     }
   }
 
   pub async fn read(&self) -> tokio::sync::RwLockReadGuard<'_, T> {
-    match tokio::time::timeout(self.timeout, self.inner.read()).await {
+    match tokio::time::timeout(DEFAULT_RWLOCK_TIMEOUT, self.inner.read()).await {
       Ok(guard) => guard,
       Err(_) => panic!(
         "Trying to obtain read lock timed out after {:?}",
-        self.timeout
+        DEFAULT_RWLOCK_TIMEOUT
       ),
     }
   }
 
   pub async fn write(&self) -> tokio::sync::RwLockWriteGuard<'_, T> {
-    match tokio::time::timeout(self.timeout, self.inner.write()).await {
+    match tokio::time::timeout(DEFAULT_RWLOCK_TIMEOUT, self.inner.write()).await {
       Ok(guard) => guard,
       Err(_) => panic!(
         "Trying to obtain read lock timed out after {:?}",
-        self.timeout
+        DEFAULT_RWLOCK_TIMEOUT
       ),
     }
   }
 
   pub async fn read_err(&self) -> Result<tokio::sync::RwLockReadGuard<'_, T>, RwLockError> {
-    match tokio::time::timeout(self.timeout, self.inner.read()).await {
+    match tokio::time::timeout(DEFAULT_RWLOCK_TIMEOUT, self.inner.read()).await {
       Ok(guard) => Ok(guard),
-      Err(_) => Err(RwLockError::ReadTimeout(self.timeout)),
+      Err(_) => Err(RwLockError::ReadTimeout(DEFAULT_RWLOCK_TIMEOUT)),
     }
   }
 
   pub async fn write_err(&self) -> Result<tokio::sync::RwLockWriteGuard<'_, T>, RwLockError> {
-    match tokio::time::timeout(self.timeout, self.inner.write()).await {
+    match tokio::time::timeout(DEFAULT_RWLOCK_TIMEOUT, self.inner.write()).await {
       Ok(guard) => Ok(guard),
-      Err(_) => Err(RwLockError::WriteTimeout(self.timeout)),
+      Err(_) => Err(RwLockError::WriteTimeout(DEFAULT_RWLOCK_TIMEOUT)),
     }
   }
 }
@@ -66,13 +65,13 @@ impl<T: ?Sized> Deref for RwLock<T> {
 
 impl<T> From<T> for RwLock<T> {
   fn from(value: T) -> Self {
-    Self::new(value, DEFAULT_RWLOCK_TIMEOUT)
+    Self::new(value)
   }
 }
 
 impl<T: Default> Default for RwLock<T> {
   fn default() -> Self {
-    Self::new(T::default(), DEFAULT_RWLOCK_TIMEOUT)
+    Self::new(T::default())
   }
 }
 
@@ -86,32 +85,33 @@ pub enum RwLockError {
 
 #[derive(Debug)]
 pub struct Mutex<T: ?Sized> {
-  inner: Box<tokio::sync::Mutex<T>>,
-  timeout: Duration,
+  inner: tokio::sync::Mutex<T>,
 }
 
 impl<T: ?Sized> Mutex<T> {
-  pub fn new(inner: T, timeout: Duration) -> Self
+  pub fn new(inner: T) -> Self
   where
     T: Sized,
   {
     Self {
-      inner: tokio::sync::Mutex::new(inner).into(),
-      timeout,
+      inner: tokio::sync::Mutex::new(inner),
     }
   }
 
   pub async fn lock(&self) -> tokio::sync::MutexGuard<'_, T> {
-    match tokio::time::timeout(self.timeout, self.inner.lock()).await {
+    match tokio::time::timeout(DEFAULT_RWLOCK_TIMEOUT, self.inner.lock()).await {
       Ok(guard) => guard,
-      Err(_) => panic!("Trying to obtain lock timed out after {:?}", self.timeout),
+      Err(_) => panic!(
+        "Trying to obtain lock timed out after {:?}",
+        DEFAULT_RWLOCK_TIMEOUT
+      ),
     }
   }
 
   pub async fn lock_err(&self) -> Result<tokio::sync::MutexGuard<'_, T>, MutexError> {
-    match tokio::time::timeout(self.timeout, self.inner.lock()).await {
+    match tokio::time::timeout(DEFAULT_RWLOCK_TIMEOUT, self.inner.lock()).await {
       Ok(guard) => Ok(guard),
-      Err(_) => Err(MutexError::LockTimeout(self.timeout)),
+      Err(_) => Err(MutexError::LockTimeout(DEFAULT_RWLOCK_TIMEOUT)),
     }
   }
 }
@@ -127,13 +127,13 @@ impl<T: ?Sized> Deref for Mutex<T> {
 
 impl<T> From<T> for Mutex<T> {
   fn from(value: T) -> Self {
-    Self::new(value, DEFAULT_RWLOCK_TIMEOUT)
+    Self::new(value)
   }
 }
 
 impl<T: Default> Default for Mutex<T> {
   fn default() -> Self {
-    Self::new(T::default(), DEFAULT_RWLOCK_TIMEOUT)
+    Self::new(T::default())
   }
 }
 
@@ -141,4 +141,19 @@ impl<T: Default> Default for Mutex<T> {
 pub enum MutexError {
   #[error("Lock timeout: {0:?}")]
   LockTimeout(Duration),
+}
+
+#[cfg(test)]
+mod test {
+  use crate::lock::RwLock;
+  use crate::preclude::Collab;
+  use std::borrow::BorrowMut;
+  use std::sync::Arc;
+
+  #[test]
+  fn trait_casting() {
+    type CollabRef = Arc<RwLock<dyn BorrowMut<Collab> + Send + Sync + 'static>>;
+    let collab = Arc::new(RwLock::new(Collab::new(0, "test", "device", vec![], false)));
+    let _collab_ref: CollabRef = collab as CollabRef;
+  }
 }
