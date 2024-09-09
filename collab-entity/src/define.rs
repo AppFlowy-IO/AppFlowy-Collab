263b03fe08fc01e61c6715519b6d661493ed1744
nathan <nathan@appflowy.io>
2024-09-09 15:14:23 +0800

    chore: database row id check

--- collab-entity/src/define.rs
+++ collab-entity/src/define.rs
@@ -13,6 +13,7 @@ pub const DATABASE_ID: &str = "id";
 pub const DATABASE_METAS: &str = "metas";
 pub const DATABASE_INLINE_VIEW: &str = "iid";
 pub const DATABASE_ROW_DATA: &str = "data";
+pub const DATABASE_ROW_ID: &str = "id";
 
 // User Awareness
 pub const USER_AWARENESS: &str = "user_awareness";
