8c5832c96d86ec621bd40dd06f635d45c97c6301
nathan <nathan@appflowy.io>
2024-09-22 17:15:42 +0800

    chore: migrate timestamp type option

--- collab-database/src/fields/type_option/date_type_option.rs
+++ collab-database/src/fields/type_option/date_type_option.rs
@@ -300,25 +300,6 @@ impl DateFormat {
   }
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct TimestampTypeOption {
-  pub date_format: DateFormat,
-  pub time_format: TimeFormat,
-  pub include_time: bool,
-  pub field_type: FieldType,
-}
-
-impl TimestampTypeOption {
-  pub fn new(field_type: FieldType, include_time: bool) -> Self {
-    Self {
-      date_format: DateFormat::default(),
-      time_format: TimeFormat::default(),
-      include_time,
-      field_type,
-    }
-  }
-}
-
 #[derive(Default, Clone, Debug, Serialize)]
 pub struct DateCellData {
   pub timestamp: Option<i64>,
