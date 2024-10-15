16db5c799191d96723db82b8299be0e8aacbe3ef
nathan <nathan@appflowy.io>
2024-10-15 20:54:53 +0800

    chore: return zip error

--- collab-importer/src/zip_tool.rs
+++ collab-importer/src/zip_tool.rs
@@ -186,9 +186,7 @@ pub async fn unzip_file(
   mut unzip_root_folder_name: Option<String>,
 ) -> Result<UnzipFile, anyhow::Error> {
   let archive = BufReader::new(archive).compat();
-  let mut reader = SeekZipFileReader::new(archive)
-    .await
-    .expect("Failed to read zip file");
+  let mut reader = SeekZipFileReader::new(archive).await?;
 
   for index in 0..reader.file().entries().len() {
     let entry = reader.file().entries().get(index).unwrap();
@@ -203,38 +201,26 @@ pub async fn unzip_file(
     // https://docs.rs/async_zip/0.0.8/src/async_zip/read/mod.rs.html#63-65
     // https://github.com/python/cpython/blob/820ef62833bd2d84a141adedd9a05998595d6b6d/Lib/zipfile.py#L528
     let entry_is_dir = entry.dir().unwrap();
-    let mut entry_reader = reader
-      .reader_without_entry(index)
-      .await
-      .expect("Failed to read ZipEntry");
+    let mut entry_reader = reader.reader_without_entry(index).await?;
 
     if entry_is_dir {
-      // The directory may have been created if iteration is out of order.
       if !path.exists() {
-        create_dir_all(&path)
-          .await
-          .expect("Failed to create extracted directory");
+        create_dir_all(&path).await?;
       }
     } else {
       // Creates parent directories. They may not exist if iteration is out of order
       // or the archive does not contain directory entries.
-      let parent = path
-        .parent()
-        .expect("A file entry should have parent directories");
-      if !parent.is_dir() {
-        create_dir_all(parent)
-          .await
-          .expect("Failed to create parent directories");
+      if let Some(parent) = path.parent() {
+        if !parent.is_dir() {
+          create_dir_all(parent).await?;
+        }
       }
       let writer = OpenOptions::new()
         .write(true)
         .create_new(true)
         .open(&path)
-        .await
-        .expect("Failed to create extracted file");
-      futures_lite::io::copy(&mut entry_reader, &mut writer.compat_write())
-        .await
-        .expect("Failed to copy to extracted file");
+        .await?;
+      futures_lite::io::copy(&mut entry_reader, &mut writer.compat_write()).await?;
     }
   }
   match unzip_root_folder_name {
