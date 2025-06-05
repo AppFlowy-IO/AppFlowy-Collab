11b16da6542f0596c0284a5a10702669141fa4a7
Lucas.Xu <lucas.xu@appflowy.io>
2025-06-05 21:02:02 +0800

    feat: add todo_list and bulleted_list

--- collab-document/src/block_parser/parsers/mod.rs
+++ collab-document/src/block_parser/parsers/mod.rs
@@ -1,9 +1,13 @@
+pub mod bulleted_list;
 pub mod heading;
 pub mod numbered_list;
 pub mod page;
 pub mod paragraph;
+pub mod todo_list;
 
+pub use bulleted_list::*;
 pub use heading::*;
 pub use numbered_list::*;
 pub use page::*;
 pub use paragraph::*;
+pub use todo_list::*;
--- collab-document/tests/block_parser/mod.rs
+++ collab-document/tests/block_parser/mod.rs
@@ -1,3 +1,5 @@
+mod bulleted_list_test;
 mod heading_test;
 mod numbered_list_test;
 mod text_utils_test;
+mod todo_list_test;
