5fdd0ce6a9e73391599651f5a0dfeee4f1f3f977
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2025-07-10 09:53:51 +0200

    chore: tests for eventual revision removal

--- collab/tests/edit_test/revision_test.rs
+++ collab/tests/edit_test/revision_test.rs
@@ -3,8 +3,6 @@ use collab::core::origin::CollabOrigin;
 use collab::entity::EncoderVersion;
 use collab::preclude::Collab;
 use serde_json::json;
-use yrs::Update;
-use yrs::updates::decoder::Decode;
 
 #[tokio::test]
 async fn create_restore_revision() {
@@ -66,9 +64,40 @@ async fn remove_revision_cleanups_deleted_data() {
   assert!(collab.restore_revision(&r1, EncoderVersion::V1).is_err()); // revision no longer exists
 
   collab.remove_revision(&r3).unwrap();
-  println!(
-    "{:#?} vs {:#?}",
-    Update::decode_v1(&state.doc_state).unwrap(),
-    Update::decode_v1(&full_state.doc_state).unwrap()
-  );
+}
+
+#[tokio::test]
+async fn remove_revision_should_eventually_remove_revision_data() {
+  let mut collab = Collab::new(1, "1", "1", default_client_id());
+  collab.insert("key", "value1");
+  let r1 = collab.create_named_revision("revision1").unwrap();
+  collab.insert("key", "value2");
+  let r2 = collab.create_named_revision("revision2").unwrap();
+
+  let state = collab.encode_collab_v2();
+  // initially we should be able to find r1 and r2 keys in the payload
+  assert!(contains(&state.doc_state, "revision1"));
+  assert!(contains(&state.doc_state, "revision2"));
+
+  // removing the first revision should not remove the data immediately,
+  // since r2 was created while r1 was still present
+  collab.remove_revision(&r1).unwrap();
+  let state = collab.encode_collab_v2();
+  assert!(contains(&state.doc_state, "revision1"));
+  assert!(!contains(&state.doc_state, "value1")); // revision is present, but its data is not
+
+  collab.insert("key", "value3");
+  let r3 = collab.create_named_revision("revision3").unwrap();
+
+  // r3 was created after r1 was removed, so it should not contain r1 data
+  // removing r2 should remove the traces of r1 as well
+  collab.remove_revision(&r2).unwrap();
+  let state = collab.encode_collab_v2();
+  assert!(!contains(&state.doc_state, "revision1")); // r1 is finally removed
+  assert!(!contains(&state.doc_state, "value2")); // r2 was removed, so its data is gone
+}
+
+fn contains<P: AsRef<[u8]>>(container: &[u8], key: P) -> bool {
+  let key = key.as_ref();
+  container.windows(key.len()).any(|window| window == key)
 }
