216e4b4c4a6b04c889665848e89d2b43c533b4f0
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-09-08 16:43:43 +0800

    feat: add person field type

---
+++ collab-database/src/fields/type_option/person_type_option.rs
@@ -0,0 +1,55 @@
+use collab::util::AnyMapExt;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::{
+  fields::{TypeOptionCellReader, TypeOptionCellWriter, TypeOptionData, TypeOptionDataBuilder},
+  rows::Cell,
+};
+
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+pub struct PersonTypeOption {
+  pub is_single_select: bool,
+  pub fill_with_creator: bool,
+  pub disable_notification: bool,
+}
+
+impl TypeOptionCellReader for PersonTypeOption {
+  fn json_cell(&self, cell: &Cell) -> Value {
+    serde_json::to_value(self.stringify_cell(cell)).unwrap_or(Value::Null)
+  }
+
+  fn stringify_cell(&self, cell_data: &Cell) -> String {
+    serde_json::to_string(cell_data).unwrap_or_default()
+  }
+
+  fn numeric_cell(&self, _cell: &Cell) -> Option<f64> {
+    None
+  }
+
+  fn convert_raw_cell_data(&self, text: &str) -> String {
+    text.to_string()
+  }
+}
+
+impl TypeOptionCellWriter for PersonTypeOption {
+  fn convert_json_to_cell(&self, _json_value: Value) -> Cell {
+    Cell::new()
+  }
+}
+
+impl From<TypeOptionData> for PersonTypeOption {
+  fn from(data: TypeOptionData) -> Self {
+    data
+      .get_as::<String>("content")
+      .map(|s| serde_json::from_str::<PersonTypeOption>(&s).unwrap_or_default())
+      .unwrap_or_default()
+  }
+}
+
+impl From<PersonTypeOption> for TypeOptionData {
+  fn from(data: PersonTypeOption) -> Self {
+    let content = serde_json::to_string(&data).unwrap_or_default();
+    TypeOptionDataBuilder::from([("content".into(), content.into())])
+  }
+}
