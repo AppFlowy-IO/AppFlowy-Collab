8c6d298a22b3562dfd3d5e07d555bc6a950fea77
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2025-10-20 05:57:01 +0200

    chore: add method to get descriptors of users making changes between two snapshots

--- collab/src/core/user_data.rs
+++ collab/src/core/user_data.rs
@@ -1,16 +1,17 @@
 use crate::core::origin::CollabOrigin;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::ops::Range;
 use std::sync::Arc;
-use tokio::sync::mpsc::UnboundedSender;
 use yrs::block::ClientID;
 use yrs::types::Change;
 use yrs::updates::decoder::Decode;
 use yrs::updates::encoder::Encode;
 use yrs::{
-  Any, Array, ArrayPrelim, ArrayRef, DeleteSet, Doc, Map, MapPrelim, MapRef, Observable, Origin,
-  Out, Subscription, Transact, TransactionMut,
+  Any, Array, ArrayPrelim, ArrayRef, DeleteSet, Doc, ID, Map, MapPrelim, MapRef, Observable,
+  Origin, Out, Snapshot, Subscription, Transact, TransactionMut,
 };
 
+/// Unique description of a user.
 pub type UserDescription = Arc<str>;
 
 #[derive(Default)]
@@ -21,6 +22,9 @@ struct State {
   current_users: Vec<UserDescription>,
 }
 
+/// Permanent user data struct that keeps track of user descriptor and their associated
+/// client ids (used for keeping track of who inserted a new data) and delete sets (used for
+/// keeping track of who deleted what).
 pub struct PermanentUserData {
   users: MapRef,
   state: Arc<parking_lot::RwLock<State>>,
@@ -43,7 +47,7 @@ impl PermanentUserData {
     let s = state.clone();
     let on_users_changed = users.observe(move |tx, e| {
       let mut lock = s.write();
-      for (key, _) in e.keys(tx) {
+      for key in e.keys(tx).keys() {
         if let Some(Out::YMap(value)) = users_clone.get(tx, key) {
           lock.action_queue.push(Action::InitUser(key.clone(), value));
         }
@@ -200,6 +204,7 @@ impl PermanentUserData {
     user_ds.squash();
   }
 
+  /// Add mapping from client id to user description.
   pub fn map_user<S: Into<UserDescription>>(
     &mut self,
     tx: &mut TransactionMut,
@@ -225,7 +230,7 @@ impl PermanentUserData {
     let state = self.state.clone();
     let users = self.users.clone();
     let description_clone = user_description.clone();
-    self.users.observe_with("pud", move |txn, e| {
+    self.users.observe_with("pud", move |txn, _| {
       let old_user = users.get(txn, &description_clone);
       if old_user != Some(Out::YMap(user.clone())) {
         // user was overridden
@@ -240,11 +245,13 @@ impl PermanentUserData {
     self.state.write().current_users.push(user_description);
   }
 
+  /// Get user description by client id.
   pub fn user_by_client_id(&self, client_id: ClientID) -> Option<UserDescription> {
     let lock = self.state.read();
     lock.clients.get(&client_id).cloned()
   }
 
+  /// Get user description by deleted block id.
   pub fn user_by_deleted_id(&self, id: &yrs::ID) -> Option<UserDescription> {
     let lock = self.state.read();
     for (description, ds) in &lock.dss {
@@ -254,6 +261,93 @@ impl PermanentUserData {
     }
     None
   }
+
+  /// Return set of users that made edits between two snapshots.
+  pub fn editors_between(&self, from: &Snapshot, to: &Snapshot) -> HashSet<UserDescription> {
+    let mut result = HashSet::new();
+
+    // get client ids that have changes between from and to snapshots
+    for (client_id, &to_clock) in to.state_map.iter() {
+      let from_clock = from.state_map.get(client_id);
+      if to_clock > from_clock {
+        if let Some(user) = self.user_by_client_id(*client_id) {
+          result.insert(user);
+        }
+      }
+    }
+
+    // also check deleted ids
+    //TODO: this is not very efficient, consider optimizing if needed
+    let ds_diff = diff_delete_sets(&from.delete_set, &to.delete_set);
+    for (client_id, ranges) in ds_diff.iter() {
+      for range in ranges.iter() {
+        for clock in range.start..range.end {
+          let id = ID::new(*client_id, clock);
+          if let Some(user) = self.user_by_deleted_id(&id) {
+            result.insert(user);
+          }
+        }
+      }
+    }
+
+    result
+  }
+}
+
+fn diff_delete_sets(old_ds: &DeleteSet, new_ds: &DeleteSet) -> DeleteSet {
+  let mut diff_ds = DeleteSet::new();
+
+  for (client_id, new_range) in new_ds.iter() {
+    let old_range = old_ds
+      .range(client_id)
+      .unwrap_or(&Default::default())
+      .clone();
+    let mut old_iter = old_range.iter();
+
+    for new_range in new_range.iter() {
+      if let Some(old_range) = old_iter.next() {
+        if intersects(new_range, old_range) {
+          // overlapping ranges, need to check for new deletions
+          if new_range.start < old_range.start {
+            // new deletion before old range
+            diff_ds.insert(
+              ID::new(*client_id, new_range.start),
+              old_range.start - new_range.start,
+            );
+          }
+          if new_range.end > old_range.end {
+            // new deletion after old range
+            diff_ds.insert(
+              ID::new(*client_id, old_range.end),
+              new_range.end - old_range.end,
+            );
+          }
+        } else if new_range.end <= old_range.start {
+          // new deletion before old range
+          diff_ds.insert(
+            ID::new(*client_id, new_range.start),
+            new_range.end - new_range.start,
+          );
+        } else if new_range.start >= old_range.end {
+          // new deletion after old range, continue to next old range
+          continue;
+        }
+      } else {
+        // all remaining new_ranges are new deletions
+        diff_ds.insert(
+          ID::new(*client_id, new_range.start),
+          new_range.end - new_range.start,
+        );
+      }
+    }
+  }
+
+  diff_ds
+}
+
+#[inline]
+fn intersects(x: &Range<u32>, y: &Range<u32>) -> bool {
+  x.start < y.end && y.start < x.end
 }
 
 #[derive(Debug)]
@@ -266,8 +360,9 @@ enum Action {
 mod test {
   use crate::core::origin::{CollabClient, CollabOrigin};
   use crate::preclude::{Doc, PermanentUserData};
+  use std::collections::HashSet;
   use yrs::updates::decoder::Decode;
-  use yrs::{ReadTxn, StateVector, Text, Transact, Update};
+  use yrs::{ReadTxn, Snapshot, StateVector, Text, Transact, Update};
 
   #[test]
   fn add_or_remove_user_mappings() {
@@ -309,6 +404,47 @@ mod test {
     assert_eq!(&*user1, "user a");
     assert_eq!(&*user2, "user b");
     assert_eq!(&*user3, "user a");
+  }
+
+  #[test]
+  fn editors_between() {
+    let origin1 = CollabOrigin::Client(CollabClient::new(1, "device-A"));
+    let origin2 = CollabOrigin::Client(CollabClient::new(2, "device-B"));
+    let d1 = Doc::new();
+    let d2 = Doc::new();
+    let mut pud1 = PermanentUserData::new(&d1, origin1);
+    let mut pud2 = PermanentUserData::new(&d2, origin2);
+
+    pud1.map_user(&mut d1.transact_mut(), d1.client_id(), "user a");
+    pud2.map_user(&mut d2.transact_mut(), d2.client_id(), "user b");
+
+    let txt1 = d1.get_or_insert_text("text");
+    let txt2 = d2.get_or_insert_text("text");
+
+    let snap1 = {
+      let mut t1 = d1.transact_mut();
+      txt1.insert(&mut t1, 0, "hello world");
+      t1.snapshot()
+    };
+
+    exchange_updates([&d1, &d2]);
+
+    let users = pud1.editors_between(&Snapshot::default(), &snap1);
+    assert_eq!(users, HashSet::from(["user a".into()]));
+
+    let snap2 = {
+      let mut t2 = d2.transact_mut();
+      txt2.remove_range(&mut t2, 4, 3); // remove "o w"
+      t2.snapshot()
+    };
+
+    exchange_updates([&d1, &d2]);
+
+    let users = pud1.editors_between(&snap1, &snap2);
+    assert_eq!(users, HashSet::from(["user b".into()]));
+
+    let users = pud1.editors_between(&Snapshot::default(), &snap2);
+    assert_eq!(users, HashSet::from(["user a".into(), "user b".into()]));
   }
 
   fn exchange_updates<const N: usize>(docs: [&Doc; N]) {
