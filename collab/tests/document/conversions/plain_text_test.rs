54cded039f3964496d3697af78658e2e5766df32
Nathan <nathan@appflowy.io>
2025-10-30 12:53:18 +0800

    chore: document to plain text refactor

--- collab/tests/document/conversions/plain_text_test.rs
+++ collab/tests/document/conversions/plain_text_test.rs
@@ -1,7 +1,10 @@
-use collab::document::{blocks::Block, document::Document};
-use nanoid::nanoid;
-
 use crate::util::DocumentTest;
+use collab::document::{
+  DefaultPlainTextResolver, PlainTextExportOptions, blocks::Block, document::Document,
+};
+use nanoid::nanoid;
+use serde_json::json;
+use std::collections::HashMap;
 
 #[test]
 fn plain_text_1_test() {
@@ -30,6 +33,122 @@ fn plain_text_1_test() {
   }
 }
 
+#[test]
+fn plain_text_mentions_default_resolver() {
+  let doc_id = "mention-default";
+  let test = DocumentTest::new(1, doc_id);
+  let mut document = test.document;
+  let page_id = document.get_page_id().unwrap();
+  let mut prev_id = "".to_string();
+
+  insert_paragraph_with_delta(
+    &mut document,
+    &page_id,
+    &mut prev_id,
+    json!([
+      {"insert": "Hello "},
+      {"insert": "$", "attributes": {"mention": {
+        "type": "person",
+        "person_id": "person-123",
+        "person_name": "Mock User",
+        "page_id": page_id
+      }}},
+      {"insert": "!"}
+    ])
+    .to_string(),
+  );
+
+  insert_paragraph_with_delta(
+    &mut document,
+    &page_id,
+    &mut prev_id,
+    json!([
+      {"insert": "Related "},
+      {"insert": "$", "attributes": {"mention": {
+        "type": "page",
+        "page_id": "doc-123"
+      }}}
+    ])
+    .to_string(),
+  );
+
+  insert_subpage_block(&mut document, &page_id, &mut prev_id, "doc-embedded");
+
+  let plain = document
+    .to_plain_text()
+    .into_iter()
+    .filter(|line| !line.trim().is_empty())
+    .collect::<Vec<_>>();
+  assert_eq!(
+    plain,
+    vec!["Hello @Mock User!", "Related [[doc-123]]", "doc-embedded"]
+  );
+}
+
+#[test]
+fn plain_text_mentions_custom_resolver() {
+  let doc_id = "mention-custom";
+  let test = DocumentTest::new(1, doc_id);
+  let mut document = test.document;
+  let page_id = document.get_page_id().unwrap();
+  let mut prev_id = "".to_string();
+
+  insert_paragraph_with_delta(
+    &mut document,
+    &page_id,
+    &mut prev_id,
+    json!([
+      {"insert": "Hello "},
+      {"insert": "$", "attributes": {"mention": {
+        "type": "person",
+        "person_id": "person-123",
+        "person_name": "Mock User",
+        "page_id": page_id
+      }}},
+      {"insert": "!"}
+    ])
+    .to_string(),
+  );
+
+  insert_paragraph_with_delta(
+    &mut document,
+    &page_id,
+    &mut prev_id,
+    json!([
+      {"insert": "Related "},
+      {"insert": "$", "attributes": {"mention": {
+        "type": "page",
+        "page_id": "doc-123"
+      }}}
+    ])
+    .to_string(),
+  );
+
+  insert_subpage_block(&mut document, &page_id, &mut prev_id, "doc-embedded");
+
+  let mut person_map = HashMap::new();
+  person_map.insert("person-123".to_string(), "Alice".to_string());
+
+  let mut doc_map = HashMap::new();
+  doc_map.insert("doc-123".to_string(), "Design Doc".to_string());
+  doc_map.insert("doc-embedded".to_string(), "Embedded Doc".to_string());
+
+  let resolver = DefaultPlainTextResolver::new()
+    .with_person_names(person_map)
+    .with_document_titles(doc_map);
+  let options = PlainTextExportOptions::from_default_resolver(resolver);
+
+  let plain = document
+    .to_plain_text_with_options(options)
+    .into_iter()
+    .filter(|line| !line.trim().is_empty())
+    .collect::<Vec<_>>();
+  assert_eq!(
+    plain,
+    vec!["Hello @Alice!", "Related [[Design Doc]]", "Embedded Doc"]
+  );
+}
+
 fn insert_paragraphs(document: &mut Document, paragraphs: Vec<String>) {
   let page_id = document.get_page_id().unwrap();
   let mut prev_id = "".to_string();
@@ -51,4 +170,61 @@ fn insert_paragraphs(document: &mut Document, paragraphs: Vec<String>) {
 
     document.apply_text_delta(&text_id, format!(r#"[{{"insert": "{}"}}]"#, paragraph));
   }
+}
+
+fn insert_paragraph_with_delta(
+  document: &mut Document,
+  page_id: &str,
+  prev_id: &mut String,
+  delta_json: String,
+) {
+  let block_id = nanoid!(6);
+  let text_id = nanoid!(6);
+  let block = Block {
+    id: block_id.clone(),
+    ty: "paragraph".to_owned(),
+    parent: page_id.to_string(),
+    children: "".to_string(),
+    external_id: Some(text_id.clone()),
+    external_type: Some("text".to_owned()),
+    data: Default::default(),
+  };
+
+  let insert_prev = if prev_id.is_empty() {
+    None
+  } else {
+    Some(prev_id.clone())
+  };
+  document.insert_block(block, insert_prev).unwrap();
+  prev_id.clone_from(&block_id);
+  document.apply_text_delta(&text_id, delta_json);
+}
+
+fn insert_subpage_block(
+  document: &mut Document,
+  page_id: &str,
+  prev_id: &mut String,
+  view_id: &str,
+) {
+  let block_id = nanoid!(6);
+  let mut data = HashMap::new();
+  data.insert("viewId".to_string(), json!(view_id));
+
+  let block = Block {
+    id: block_id.clone(),
+    ty: "sub_page".to_owned(),
+    parent: page_id.to_string(),
+    children: "".to_string(),
+    external_id: None,
+    external_type: None,
+    data,
+  };
+
+  let insert_prev = if prev_id.is_empty() {
+    None
+  } else {
+    Some(prev_id.clone())
+  };
+  document.insert_block(block, insert_prev).unwrap();
+  prev_id.clone_from(&block_id);
 }
